42a9a7c353a26a00ae6715425605e345184addb7
User <sofyamilich@gmail.com>
2024-09-30 13:22:30 +0300

    Добавила уведомление о сильных колебаниях за день: сравнивает среднее значение за период с максимумами и минимумами за день в %

--- analysis_data_mean.py
+++ analysis_data_mean.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 
 def mean_closing_price(data):
 
@@ -10,6 +7,7 @@ def mean_closing_price(data):
 
     return data['Close'].mean()
 
+
 def display_mean_closing_price(data):
 
     # Вычисляем и выводим среднюю цену закрытия акций за период
@@ -17,3 +15,18 @@ def display_mean_closing_price(data):
     print(f"Средняя цена закрытия акций за период: {mean_price}")
 
 
+def notify_if_strong_fluctuations(data, threshold_options):
+    # Анализируем данные и уведомляем пользователя, если цена акций колебалась более чем на заданный процент за период
+    # data - DataFrame с данными об акциях
+    # threshold - заданный порог в процентах
+
+    max_close = data['Close'].max()  # максимальная цена закрытия
+    print(f'Максимальная цена закрытия {max_close}')
+    min_close = data['Close'].min()  # минимальная цена закрытия
+    print(f'Минимальная цена закрытия {min_close}')
+    fluctuation = (max_close - min_close) / data['Close'].mean() * 100  # колебание в процентах
+
+    for threshold, description in threshold_options.items():
+        if fluctuation > threshold:
+            print(f"{description} цены акций за день, на {fluctuation} %")
+            break
--- main.py
+++ main.py
@@ -1,6 +1,103 @@
+# import data_download as dd
+# import data_plotting as dplt
+# import analysis_data_mean as da
+#
+# # main.py:
+# # Файл является точкой входа в программу.
+# # Запрашивает у пользователя тикер акции и временной период, загружает данные, обрабатывает
+# # их и выводит результаты в виде графика.
+#
+# # Основная функция, управляющая процессом загрузки, обработки данных и их визуализации. Запрашивает у пользователя ввод данных, вызывает функции загрузки и обработки
+# # данных, а затем передаёт результаты на визуализацию.
+# def main():
+#     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
+#     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+#     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
+#
+#     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+#     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+#
+#     # Fetch stock data
+#     # Загружаем данные об акциях
+#     stock_data = dd.fetch_stock_data(ticker, period)
+#
+#     # Add moving average to the data
+#     # Добавим скользящую среднюю к данным
+#     stock_data = dd.add_moving_average(stock_data)
+#
+#     # Plot the data
+#     # Построим график
+#     dplt.create_and_save_plot(stock_data, ticker, period)
+#
+#     # Вычислим и выведем среднюю цену закрытия акций
+#     da.display_mean_closing_price(stock_data)
+#
+#     # Уведомляем о сильных колебаниях
+#     threshold_options = {
+#         1: "небольшие колебания цены акций на 1%",
+#         2: "средние колебания цены акций от 1 до 2 %",
+#         3: "значительные колебания цены акций от 2 до 3 %",
+#         5: "очень значительные колебания цены акций от 3 и выше %",
+#     }
+#
+#     for threshold, description in threshold_options.items():
+#         da.notify_if_strong_fluctuations(stock_data, threshold)
+#
+#
+# if __name__ == "__main__":
+#     main()
+# import data_download as dd
+# import data_plotting as dplt
+# import analysis_data_mean as da
+#
+# # main.py:
+# # Файл является точкой входа в программу.
+# # Запрашивает у пользователя тикер акции и временной период, загружает данные, обрабатывает
+# # их и выводит результаты в виде графика.
+#
+# # Основная функция, управляющая процессом загрузки, обработки данных и их визуализации. Запрашивает у пользователя ввод данных, вызывает функции загрузки и обработки
+# # данных, а затем передаёт результаты на визуализацию.
+# def main():
+#     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
+#     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+#     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
+#
+#     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+#     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+#
+#     # Fetch stock data
+#     # Загружаем данные об акциях
+#     stock_data = dd.fetch_stock_data(ticker, period)
+#
+#     # Add moving average to the data
+#     # Добавим скользящую среднюю к данным
+#     stock_data = dd.add_moving_average(stock_data)
+#
+#     # Plot the data
+#     # Построим график
+#     dplt.create_and_save_plot(stock_data, ticker, period)
+#
+#     # Вычислим и выведем среднюю цену закрытия акций
+#     da.display_mean_closing_price(stock_data)
+#
+#     # Уведомляем о сильных колебаниях
+#     threshold_options = {
+#         1: "небольшие колебания цены акций",
+#         2: "средние колебания цены акций",
+#         3: "значительные колебания цены акций",
+#         5: "очень значительные колебания цены акций",
+#     }
+#
+#     for threshold, description in threshold_options.items():
+#         da.notify_if_strong_fluctuations(stock_data, threshold)
+#
+#
+# if __name__ == "__main__":
+#     main()
 import data_download as dd
 import data_plotting as dplt
 import analysis_data_mean as da
+import analysis_data_mean
 
 # main.py:
 # Файл является точкой входа в программу.
@@ -22,15 +119,24 @@ def main():
     stock_data = dd.fetch_stock_data(ticker, period)
 
     # Add moving average to the data
+    # Добавим скользящую среднюю к данным
     stock_data = dd.add_moving_average(stock_data)
 
     # Plot the data
+    # Построим график
     dplt.create_and_save_plot(stock_data, ticker, period)
 
-    # Вычисляем и выводим среднюю цену закрытия акций
+    # Вычислим и выведем среднюю цену закрытия акций
     da.display_mean_closing_price(stock_data)
 
+    # Уведомляем о сильных колебаниях
+    threshold_options = {
+        5: "небольшие колебания цены акций",
+        10: "средние колебания цены акций",
+        20: "значительные колебания цены акций",
+    }
 
+    da.notify_if_strong_fluctuations(stock_data, threshold_options)
 
 
 if __name__ == "__main__":
