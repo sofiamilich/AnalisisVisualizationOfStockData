eda5e66c2bf44c51d6db9e2fa0420282a33fd869
User <sofyamilich@gmail.com>
2024-10-09 19:57:56 +0300

    3. Добавила выгрузку в csv и ексель: 1) загруженные с сайта данные об акциях  2) выгрузку в общий файл, куда в порядке сортировки по дате падают все важные моменты, которые мы выводили в консоль, а именно: дату, тикер, период, среднюю цену, максимальную цену, минимальную цену, колебания в %

--- data_download.py
+++ data_download.py
@@ -1,20 +1,159 @@
 import yfinance as yf
+import pandas as pd
+import os
 
 # data_download.py:
 # Файл отвечает за загрузку данных об акциях.
 # Содержит функции для извлечения данных об акциях из интернета и расчёта скользящего среднего.
 
 
-# Получаем исторические данные об акциях для указанного тикера и временного периода.
-# Возвращает DataFrame с данными.
 def fetch_stock_data(ticker, period='1mo'):
+    """
+    Получаем исторические данные об акциях для указанного тикера и временного периода.
+    Возвращает DataFrame с данными.
+    ticker: имя тикера, который ввел пользователь
+    period: период, который запросил пользователь
+    """
     stock = yf.Ticker(ticker)
     data = stock.history(period=period)
     return data
 
 
-# Добавляем в DataFrame колонку со скользящим средним,
-# рассчитанным на основе цен закрытия.
 def add_moving_average(data, window_size=5):
+    """
+    Добавляем в DataFrame колонку со скользящим средним,
+    рассчитанным на основе цен закрытия.
+    Параметры:
+    data (DataFrame): данные
+    window_size: размер окна
+    """
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
+
+
+def export_data_to_csv(data, filename):
+    """
+    Экспортирует данные в файл CSV.
+
+    Параметры:
+    data (DataFrame): данные для экспорта
+    filename (str): имя файла для сохранения данных
+    """
+    data.to_csv(filename, index=False)
+    print(f"Данные экспортированы в {filename}")
+
+
+def export_data_to_excel(data, filename):
+    """
+    Экспортирует данные в файл Excel.
+
+    Параметры:
+    data (DataFrame): данные для экспорта
+    filename (str): имя файла для сохранения данных
+    """
+    data.to_excel(filename, index=False)
+    print(f"Данные экспортированы в {filename}")
+
+
+def collect_data(stock_data, ticker, period):
+    """
+    Собирает данные в таблицу.
+
+    Параметры:
+    stock_data (DataFrame): Данные об акциях
+    ticker (str): Тикер, введенный пользователем
+    period (str): Период, введенный пользователем
+    """
+    data = {
+        'Дата выгрузки': [pd.Timestamp.now()],
+        'Тикер': [ticker],
+        'Период': [period],
+        'Средняя цена закрытия': [stock_data['Close'].mean()],
+        'Максимальная цена закрытия': [stock_data['Close'].max()],
+        'Минимальная цена закрытия': [stock_data['Close'].min()],
+        'Колебания цены': [(stock_data['Close'].max() - stock_data['Close'].min()) / stock_data['Close'].mean()]
+    }
+    df = pd.DataFrame(data)
+    return df
+
+
+# def collect_console_data(stock_data, ticker, period):
+#     """
+#     Собирает данные в таблицу для консоли.
+#
+#     Параметры:
+#     stock_data (DataFrame): Данные об акциях
+#     ticker (str): Тикер, введенный пользователем
+#     period (str): Период, введенный пользователем
+#     """
+#     data = {
+#         'Дата выгрузки': [pd.Timestamp.now()],
+#         'Тикер': [ticker],
+#         'Период': [period],
+#         'Средняя цена закрытия': [stock_data['Close'].mean()],
+#         'Максимальная цена закрытия': [stock_data['Close'].max()],
+#         'Минимальная цена закрытия': [stock_data['Close'].min()],
+#         'Колебания цены': [(stock_data['Close'].max() - stock_data['Close'].min()) / stock_data['Close'].mean()]
+#     }
+#     df = pd.DataFrame(data)
+#     return df
+
+
+def export_console_data_to_csv(data, filename):
+    """
+    Экспортирует данные, которые выводили в консоль в файл CSV.
+
+    Параметры:
+    data (DataFrame): данные для экспорта
+    filename (str): имя файла для сохранения данных
+    """
+    data.to_csv(filename, index=False)
+    print(f"Данные экспортированы в {filename}")
+
+
+def export_console_data_to_excel(data, filename):
+    """
+    Экспортирует данные из консоли в файл Excel.
+
+    Параметры:
+    data (DataFrame): данные для экспорта
+    filename (str): имя файла для сохранения данных
+    """
+    data.to_excel(filename, index=False)
+    print(f"Данные экспортированы в {filename}")
+
+
+def collect_console_data_new(stock_data, ticker, period, filename):
+    """
+    Собирает данные из консоли в общую таблицу, которая сортируется по дате сбора данных.
+
+    Параметры:
+    stock_data (DataFrame): данные об акциях
+    ticker (str): тикер, введенный пользователем
+    period (str): период, введенный пользователем
+    filename (str): имя файла для сохранения данных
+    """
+    data = {
+        'Дата выгрузки': [pd.Timestamp.now()],
+        'Тикер': [ticker],
+        'Период': [period],
+        'Средняя цена закрытия': [stock_data['Close'].mean()],
+        'Максимальная цена закрытия': [stock_data['Close'].max()],
+        'Минимальная цена закрытия': [stock_data['Close'].min()],
+        'Колебания цены': [(stock_data['Close'].max() - stock_data['Close'].min()) / stock_data['Close'].mean()]
+    }
+    df = pd.DataFrame(data)
+    df['Дата выгрузки'] = pd.to_datetime(df['Дата выгрузки'])  # Преобразуем к типу datetime
+
+    # Проверяем, существует ли файл
+    if os.path.exists(filename):
+        # Если файл существует, читаем существующие данные и добавляем новые
+        existing_data = pd.read_csv(filename)
+        existing_data['Дата выгрузки'] = pd.to_datetime(existing_data['Дата выгрузки'],
+                                                        errors='coerce')  # Преобразуем к типу datetime
+        df = pd.concat([existing_data, df])
+        df['Дата выгрузки'] = pd.to_datetime(df['Дата выгрузки'], errors='coerce')  # Преобразуем к типу datetime
+        df = df.sort_values(by='Дата выгрузки')  # Сортируем по дате выгрузки
+
+    df.to_csv(filename, index=False)
+    print(f"Данные экспортированы в {filename}")
--- main.py
+++ main.py
@@ -1,115 +1,26 @@
-# import data_download as dd
-# import data_plotting as dplt
-# import analysis_data_mean as da
-#
-# # main.py:
-# # Файл является точкой входа в программу.
-# # Запрашивает у пользователя тикер акции и временной период, загружает данные, обрабатывает
-# # их и выводит результаты в виде графика.
-#
-# # Основная функция, управляющая процессом загрузки, обработки данных и их визуализации. Запрашивает у пользователя ввод данных, вызывает функции загрузки и обработки
-# # данных, а затем передаёт результаты на визуализацию.
-# def main():
-#     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
-#     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
-#     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
-#
-#     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
-#     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
-#
-#     # Fetch stock data
-#     # Загружаем данные об акциях
-#     stock_data = dd.fetch_stock_data(ticker, period)
-#
-#     # Add moving average to the data
-#     # Добавим скользящую среднюю к данным
-#     stock_data = dd.add_moving_average(stock_data)
-#
-#     # Plot the data
-#     # Построим график
-#     dplt.create_and_save_plot(stock_data, ticker, period)
-#
-#     # Вычислим и выведем среднюю цену закрытия акций
-#     da.display_mean_closing_price(stock_data)
-#
-#     # Уведомляем о сильных колебаниях
-#     threshold_options = {
-#         1: "небольшие колебания цены акций на 1%",
-#         2: "средние колебания цены акций от 1 до 2 %",
-#         3: "значительные колебания цены акций от 2 до 3 %",
-#         5: "очень значительные колебания цены акций от 3 и выше %",
-#     }
-#
-#     for threshold, description in threshold_options.items():
-#         da.notify_if_strong_fluctuations(stock_data, threshold)
-#
-#
-# if __name__ == "__main__":
-#     main()
-# import data_download as dd
-# import data_plotting as dplt
-# import analysis_data_mean as da
-#
-# # main.py:
-# # Файл является точкой входа в программу.
-# # Запрашивает у пользователя тикер акции и временной период, загружает данные, обрабатывает
-# # их и выводит результаты в виде графика.
-#
-# # Основная функция, управляющая процессом загрузки, обработки данных и их визуализации. Запрашивает у пользователя ввод данных, вызывает функции загрузки и обработки
-# # данных, а затем передаёт результаты на визуализацию.
-# def main():
-#     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
-#     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
-#     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
-#
-#     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
-#     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
-#
-#     # Fetch stock data
-#     # Загружаем данные об акциях
-#     stock_data = dd.fetch_stock_data(ticker, period)
-#
-#     # Add moving average to the data
-#     # Добавим скользящую среднюю к данным
-#     stock_data = dd.add_moving_average(stock_data)
-#
-#     # Plot the data
-#     # Построим график
-#     dplt.create_and_save_plot(stock_data, ticker, period)
-#
-#     # Вычислим и выведем среднюю цену закрытия акций
-#     da.display_mean_closing_price(stock_data)
-#
-#     # Уведомляем о сильных колебаниях
-#     threshold_options = {
-#         1: "небольшие колебания цены акций",
-#         2: "средние колебания цены акций",
-#         3: "значительные колебания цены акций",
-#         5: "очень значительные колебания цены акций",
-#     }
-#
-#     for threshold, description in threshold_options.items():
-#         da.notify_if_strong_fluctuations(stock_data, threshold)
-#
-#
-# if __name__ == "__main__":
-#     main()
 import data_download as dd
 import data_plotting as dplt
 import analysis_data_mean as da
-import analysis_data_mean
+# import analysis_data_mean
+import pandas as pd
 
-# main.py:
-# Файл является точкой входа в программу.
-# Запрашивает у пользователя тикер акции и временной период, загружает данные, обрабатывает
-# их и выводит результаты в виде графика.
+"""
+main.py:
+Файл является точкой входа в программу.
+Запрашивает у пользователя тикер акции и временной период, загружает данные, обрабатывает
+их и выводит результаты в виде графика.
 
-# Основная функция, управляющая процессом загрузки, обработки данных и их визуализации. Запрашивает у пользователя ввод данных, вызывает функции загрузки и обработки
-# данных, а затем передаёт результаты на визуализацию.
+Основная функция, управляющая процессом загрузки, обработки данных и их визуализации.
+Запрашивает у пользователя ввод данных, вызывает функции загрузки и обработки
+данных, а затем передаёт результаты на визуализацию.
+"""
+
 def main():
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
-    print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
-    print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
+    print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc),"
+          " GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+    print("Общие периоды времени для данных о запасах включают: "
+          "1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
@@ -137,6 +48,22 @@ def main():
     }
 
     da.notify_if_strong_fluctuations(stock_data, threshold_options)
+
+    # Экспорт данных в CSV
+    dd.export_data_to_csv(stock_data, f"{ticker}_{period}_stock_data.csv")
+
+    # Экспорт данных в Excel
+    dd.export_data_to_excel(stock_data, f"{ticker}_{period}_stock_data.xlsx")
+
+    # # Создание таблицы с данными для консоли
+    # data_table = dd.collect_console_data_new(stock_data, ticker, period)
+
+    # Создание таблицы с данными для консоли
+    filename = "console_data_all.csv"
+    dd.collect_console_data_new(stock_data, ticker, period, filename)
+
+    # Экспорт данных в Excel для консоли
+    dd.export_console_data_to_excel(pd.read_csv(filename), "console_data_all.xlsx")
 
 
 if __name__ == "__main__":
