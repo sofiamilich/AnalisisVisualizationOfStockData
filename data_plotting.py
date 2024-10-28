5dac21e1b8b324a002edac391520c677ba342aeb
User <sofyamilich@gmail.com>
2024-10-28 17:57:26 +0300

    10. Добавила вывод в интерактивный график, используя библиотеку plotly. Обновила requirements

--- data_plotting.py
+++ data_plotting.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 import seaborn as sns
 from data_download import calculate_standard_deviation
+import plotly.graph_objs as go
+import plotly.offline as pyo
 
 # Устанавливаем стиль seaborn
 sns.set()
@@ -39,7 +41,7 @@ def create_and_save_plot(data, ticker, period, style='classic', filename=None):
     else:
         if not pd.api.types.is_datetime64_any_dtype(data['Date']):
             data['Date'] = pd.to_datetime(data['Date'])
-        plt.plot(data['Date'], data['Close'], label='Close Price')
+        plt.plot(data['Date'], data['Close'], label='Close Price', color='red')
         plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
         if 'RSI' in data.columns:
             plt.plot(data['Date'], data['RSI'], label='RSI')
@@ -67,3 +69,114 @@ def create_and_save_plot(data, ticker, period, style='classic', filename=None):
     export_path = os.path.join(export_folder, filename)
     plt.savefig(export_path)
     print(f"График сохранен как {export_path}")
+
+
+def create_interactive_plot(data, ticker, period):
+    """Создаёт интерактивный график, отображающий цены закрытия и скользящие средние."""
+
+    # Создаем график
+    fig = go.Figure()
+
+    # Добавляем цены закрытия
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Close Price', line=dict(color='red')))
+
+    # Добавляем скользящее среднее
+    if 'Moving_Average' in data.columns:
+        fig.add_trace(go.Scatter(x=data.index, y=data['Moving_Average'], mode='lines', name='Moving Average',
+                                 line=dict(color='blue')))
+
+    # Добавляем RSI, если есть
+    if 'RSI' in data.columns:
+        fig.add_trace(go.Scatter(x=data.index, y=data['RSI'], mode='lines', name='RSI', line=dict(color='green')))
+
+    # Добавляем MACD, если есть
+    if 'MACD' in data.columns and 'Signal' in data.columns:
+        fig.add_trace(go.Scatter(x=data.index, y=data['MACD'], mode='lines', name='MACD', line=dict(color='orange')))
+        fig.add_trace(
+            go.Scatter(x=data.index, y=data['Signal'], mode='lines', name='Signal', line=dict(color='purple')))
+
+    # Обновляем макет графика
+    fig.update_layout(title=f"{ticker} Цена акций с течением времени",
+                      xaxis_title="Дата",
+                      yaxis_title="Цена",
+                      legend_title="Легенда")
+
+    # Сохраняем график в файл HTML
+    export_folder = "export/interactive_chart"
+    if not os.path.exists(export_folder):
+        os.makedirs(export_folder)
+    export_path = os.path.join(export_folder, f"{ticker}_{period}_interactive_chart.html")
+    pyo.plot(fig, filename=export_path, auto_open=True)
+    print(f"Интерактивный график сохранен как {export_path}")
+
+# Код без интерактивного графика
+
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import os
+# import seaborn as sns
+# from data_download import calculate_standard_deviation
+#
+# # Устанавливаем стиль seaborn
+# sns.set()
+#
+#
+# # data_plotting.py:
+# # Отвечает за визуализацию данных.
+# # Содержит функции для создания и сохранения графиков цен закрытия
+# # и скользящих средних.
+#
+#
+# # Создаём график, отображающий цены закрытия и скользящие средние.
+# # Предоставляет возможность сохранения графика в файл. Параметр filename -
+# # если он не указан, имя файла генерируется автоматически.
+# def create_and_save_plot(data, ticker, period, style='classic', filename=None):
+#     """Создаёт график, отображающий цены закрытия и скользящие средние."""
+#
+#     plt.style.use(style)  # Применяем выбранный стиль
+#     plt.figure(figsize=(10, 6))
+#
+#     if 'Date' not in data:
+#         if pd.api.types.is_datetime64_any_dtype(data.index):
+#             dates = data.index.to_numpy()
+#             plt.plot(dates, data['Close'].values, label='Close Price')
+#             plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
+#             if 'RSI' in data.columns:
+#                 plt.plot(dates, data['RSI'].values, label='RSI')
+#             if 'MACD' in data.columns and 'Signal' in data.columns:
+#                 plt.plot(dates, data['MACD'].values, label='MACD')
+#                 plt.plot(dates, data['Signal'].values, label='Signal')
+#         else:
+#             print("Информация о дате отсутствует или не имеет распознаваемого формата.")
+#             return
+#     else:
+#         if not pd.api.types.is_datetime64_any_dtype(data['Date']):
+#             data['Date'] = pd.to_datetime(data['Date'])
+#         plt.plot(data['Date'], data['Close'], label='Close Price', color='red')
+#         plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
+#         if 'RSI' in data.columns:
+#             plt.plot(data['Date'], data['RSI'], label='RSI')
+#         if 'MACD' in data.columns and 'Signal' in data.columns:
+#             plt.plot(data['Date'], data['MACD'], label='MACD')
+#             plt.plot(data['Date'], data['Signal'], label='Signal')
+#
+#     # Добавляем стандартное отклонение на график
+#     std_dev = calculate_standard_deviation(data)
+#     mean_price = data['Close'].mean()
+#     plt.axhline(mean_price + std_dev, color='red', linestyle='--', label='Mean + Std Dev')
+#     plt.axhline(mean_price - std_dev, color='blue', linestyle='--', label='Mean - Std Dev')
+#
+#     plt.title(f"{ticker} Цена акций с течением времени")
+#     plt.xlabel("Дата")
+#     plt.ylabel("Цена")
+#     plt.legend()
+#
+#     if filename is None:
+#         filename = f"{ticker}_{period}_stock_price_chart.png"
+#
+#     export_folder = "export/chart"
+#     if not os.path.exists(export_folder):
+#         os.makedirs(export_folder)
+#     export_path = os.path.join(export_folder, filename)
+#     plt.savefig(export_path)
+#     print(f"График сохранен как {export_path}")
--- main.py
+++ main.py
@@ -43,8 +43,12 @@ def main():
                   " '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', "
                   "'grayscale', 'seaborn-v0_8'): ")
 
-    # Plot the data dplt.create_and_save_plot(stock_data, ticker, period)
-    dplt.create_and_save_plot(stock_data, ticker, period, style)
+    # Закомментируем вызов функции, чтобы создавать интерактивный график через plotly
+    # # Plot the data dplt.create_and_save_plot(stock_data, ticker, period)
+    # dplt.create_and_save_plot(stock_data, ticker, period, style)
+
+    # Заменим вызов прошлой функции на эту строку
+    dplt.create_interactive_plot(stock_data, ticker, period)
 
     # Calculate and display mean closing price
     da.display_mean_closing_price(stock_data)
@@ -81,3 +85,88 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Код без вывода интерактивного графика
+# import data_download as dd
+# import analysis_data_mean as da
+# import os
+# import data_plotting as dplt
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+#
+#
+# def main():
+#     # Выводим доступные стили
+#     print("Доступные стили для графика:", plt.style.available)
+#     # Устанавливаем стиль seaborn
+#     sns.set()
+#     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
+#     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), "
+#           "GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+#     print("Общие периоды времени для данных о запасах включают: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max.")
+#     print("Вы также можете ввести пользовательские даты в формате YYYY-MM-DD.")
+#
+#     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc): ")
+#     period = input("Введите период для данных (например, '1mo' для одного месяца или"
+#                    " 'YYYY-MM-DD' - дату начала периода, который хотите увидеть): ")
+#
+#     # Проверяем, являются ли введенные данные датами или периодом
+#     if len(period) == 10 and period[4] == '-' and period[7] == '-':
+#         start_date = period
+#         end_date = input("Введите дату окончания (в формате YYYY-MM-DD): ")
+#         stock_data = dd.fetch_stock_data(ticker, start=start_date, end=end_date)
+#     else:
+#         stock_data = dd.fetch_stock_data(ticker, period)
+#
+#     # Add moving average to the data
+#     stock_data = dd.add_moving_average(stock_data)
+#
+#     # Let's add RSI
+#     stock_data = dd.calculate_rsi(stock_data)
+#
+#     # Adding a function call
+#     stock_data = dd.calculate_macd(stock_data)
+#
+#     # User selects the style of the plot
+#     style = input("Выберите стиль графика (например, 'Solarize_Light2', '_classic_test_patch', '_mpl-gallery',"
+#                   " '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', "
+#                   "'grayscale', 'seaborn-v0_8'): ")
+#
+#     # Plot the data dplt.create_and_save_plot(stock_data, ticker, period)
+#     dplt.create_and_save_plot(stock_data, ticker, period, style)
+#
+#     # Calculate and display mean closing price
+#     da.display_mean_closing_price(stock_data)
+#
+#     # Calculate and display standard deviation
+#     da.display_standard_deviation(stock_data)
+#
+#     # Notify if strong fluctuations
+#     threshold_options = {
+#         5: "Небольшие колебания цены акций",
+#         10: "Средние колебания цены акций",
+#         20: "Значительные колебания цены акций",
+#     }
+#     da.notify_if_strong_fluctuations(stock_data, threshold_options)
+#
+#     # Export data to CSV
+#     export_folder = "export"
+#     if not os.path.exists(export_folder):
+#         os.makedirs(export_folder)
+#     filename = f"{ticker}_{period}_stock_data.csv"
+#     export_path = os.path.join(export_folder, filename)
+#     dd.export_data_to_csv(stock_data, export_path)
+#
+#     # Export data to Excel
+#     filename = f"{ticker}_{period}_stock_data.xlsx"
+#     export_path = os.path.join(export_folder, filename)
+#     dd.export_data_to_excel(stock_data, export_path)
+#
+#     # Create table with data for console
+#     filename = "console_data_all.csv"
+#     export_path = os.path.join(export_folder, filename)
+#     dd.collect_console_data_new(stock_data, ticker, period, export_path)
+#
+#
+# if __name__ == "__main__":
+#     main()
